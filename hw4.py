52b8446c8b5639e73f085e8446cccedadd4b5e01
abeython <abeython@umich.edu>
2021-09-30 20:36:17 -0400

    finished main, finished main test cases, finished other test cases

--- hw4.py
+++ hw4.py
@@ -77,10 +77,10 @@ class Cashier:
 class Stall:
     
     #constructor
-    def __init__(self, name, inventory):
+    def __init__(self, name, inventory, cost = 7):
         self.name = name
         self.inventory = inventory
-        self.cost = 7
+        self.cost = cost
         self.earnings = 0
     
     #process_order method
@@ -88,24 +88,31 @@ class Stall:
         if self.has_item(food, quantity) == True:
             #update the stock of the inventory
             self.inventory[food] = self.inventory[food] - quantity
-            #update the earnings
-            self.earnings = self.compute_cost(quantity)
-        else 
-            return "ERRRRRROR: THIS STALL DOESN'T HAVE ENOUGH" + name
+            
+        else:
+            return "ERRRRRROR: THIS STALL DOESN'T HAVE ENOUGH" + food
 
     #has_item method
     def has_item(self, food, quantity):
-        #if enough food left
-        if quantity <= self.inventory.get(food):
-            return True
-        #else not enough
-        else:
-            return False 
+
+        #check if food is in inventory
+        if self.inventory.get(food) != None:
+            #if enough food left
+            if quantity <= self.inventory.get(food):
+                return True
+            #else not enough
+            else:
+                return False 
+
+        #food is not in inventory
+        else: 
+            return False
+
     
     #stock_up method
     def stock_up(self, food, quantity):
         #if food exists (has at the very least 1) then add quantity
-        if self.has_item(food, 1)
+        if self.inventory.get(food) != "None":
             self.inventory[food] = self.inventory[food] + quantity
         #if food doesn't exist then add key food with default val quantity
         else:
@@ -118,7 +125,10 @@ class Stall:
     #str
     def __str__(self):
         inventory_list = list(self.inventory)
-        return "Hello, we are " + self.name + ". This is current menu " + inventory_list = ". We charge $" + self.cost + " per item. We have $" + self.earnings + " in total."
+        string = "Hello, we are " + self.name + ". This is current menu " 
+        + str(inventory_list) + ". We charge $" + self.cost + " per item. We have $" + self.earnings + " in total."
+
+        return string
         
         
 
@@ -198,8 +208,8 @@ class TestAllMethods(unittest.TestCase):
     def test_compute_cost(self):
         #what's wrong with the following statements?
         #can you correct them?
-        self.assertEqual(self.s1.compute_cost(self.s1,5), 51)
-        self.assertEqual(self.s3.compute_cost(self.s3,6), 45)
+        self.assertEqual(self.s1.compute_cost(5), 50) #was 51
+        self.assertEqual(self.s3.compute_cost(6), 42) #was 45
 
 	# Check that the stall can properly see when it is empty
     def test_has_item(self):
@@ -208,30 +218,37 @@ class TestAllMethods(unittest.TestCase):
         # Test to see if has_item returns True when a stall has enough items left
         # Please follow the instructions below to create three different kinds of test cases 
         # Test case 1: the stall does not have this food item: 
+        self.assertFalse(self.s1.has_item("Sub", 10))
         
         # Test case 2: the stall does not have enough food item: 
+        self.assertFalse(self.s2.has_item("Burger", 50))
         
         # Test case 3: the stall has the food item of the certain quantity: 
-        pass
+        self.assertTrue(self.s3.has_item("Taco", 10))
+        
 
 	# Test validate order
     def test_validate_order(self):
 		# case 1: test if a customer doesn't have enough money in their wallet to order
-
+        self.assertEqual(self.f2.validate_order(self.c1, self.s1, "Burger", 20), "Don't have enough money for that :( Please reload more money!")
 		# case 2: test if the stall doesn't have enough food left in stock
-
+        self.f1.reload_money(400)
+        self.assertEqual(self.f1.validate_order(self.c2, self.s3, "Taco", 60), "Our stall has run out of " + "Taco" + " :( Please try a different stall!")
 		# case 3: check if the cashier can order item from that stall
-        pass
+        new_inventory = {"Sandwich":20, "Sub":10}
+        self.s4 = Stall("Barbeque", new_inventory)
+        self.assertEqual(self.f2.validate_order(self.c1, self.s4, "Sandwich", 2), "Sorry, we don't have that vendor stall. Please try a different one.")
 
     # Test if a customer can add money to their wallet
     def test_reload_money(self):
-        pass
+        self.f2.reload_money(200)
+        self.assertEqual(self.f2.wallet, 350)
     
 ### Write main function
 def main():
     #Create different objects 
     #inventories
-    first_inventory = {"Carrot":50, "Apple":75, "Orange":30}
+    first_inventory = {"Carrot":50, "Apple":75, "Orange":5}
     second_inventory = {"Bread":40, "Rice":20, "Soup":80}
 
     #customers
@@ -244,22 +261,23 @@ def main():
     stall_pink = Stall("pink", second_inventory)
 
     #cashiers
-    directory = [first_inventory, second_inventory]
-    cashier_square = Cashier("square", directory)
-    cashier_rectangle = Cashier("rectangle", directory)
+    complete_directory = [stall_blue, stall_pink]
+    incomplete_directory = [stall_blue]
+    cashier_square = Cashier("square", complete_directory)
+    cashier_rectangle = Cashier("rectangle", incomplete_directory)
 
 
     #Try all cases in the validate_order function
     #Below you need to have *each customer instance* try the four cases
     #case 1: the cashier does not have the stall 
-    
+    bob.validate_order(cashier_rectangle, stall_pink, "Rice", 2)
     #case 2: the casher has the stall, but not enough ordered food or the ordered food item
-    
+    rainn.validate_order(cashier_square, stall_blue, "Orange", 9)
     #case 3: the customer does not have enough money to pay for the order: 
-    
+    edgar.validate_order(cashier_square, stall_pink, "Bread", 17)
     #case 4: the customer successfully places an order
-
-    pass
+    rainn.validate_order(cashier_rectangle, stall_blue, "Apple", 5)
+    
 
 if __name__ == "__main__":
 	main()
